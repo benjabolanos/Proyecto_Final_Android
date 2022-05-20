16c117e7cc050090f38d81c7f747197c07034ff7
=benjabolanos <=>
2022-05-20 10:50:31 -0700

    2022.05.20

--- app/src/main/java/uabc/ic/benjaminbolanos/proyectofinalandroid/MainActivity.kt
+++ app/src/main/java/uabc/ic/benjaminbolanos/proyectofinalandroid/MainActivity.kt
@@ -3,11 +3,13 @@ package uabc.ic.benjaminbolanos.proyectofinalandroid
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
@@ -28,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //searchPlayer()
+        queue = Volley.newRequestQueue(this)
         playerNameText = findViewById(R.id.player_name_text)
         playerSearchView = findViewById(R.id.player_search)
         playerSearchButton = findViewById(R.id.player_search_button)
@@ -36,11 +39,10 @@ class MainActivity : AppCompatActivity() {
             //playerNameText.text = summonerName
             searchPlayer(summonerName)
         }
+        getArray()
     }
 
     private fun searchPlayer(summonerName:String){
-
-        queue = Volley.newRequestQueue(this)
 
         val stringRequst = object: StringRequest(Request.Method.GET, summonerURL+summonerName,
             Response.Listener { response ->
@@ -54,11 +56,38 @@ class MainActivity : AppCompatActivity() {
         {
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
-                headers["X-Riot-Token"] = "RGAPI-608bc761-a9a4-4d35-aaed-6368f008041b"
+                headers["X-Riot-Token"] = "RGAPI-c96ebbd1-05e2-448e-88ea-4dc33668a860"
                 return headers
             }
         }
         queue.add(stringRequst)
+    }
+
+    private fun getArray(){
+        Log.i("cadena", "Get array")
+        val gameListURL = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/BNWii2rE7g54BV8c7KPYxwGdYvYXfCMuPy_I1_cWl7G4filqaJEghPRLpng1vwAB_bhEW8bRlxHjgw/ids?start=0&count=10"
+        val jsonRequest = object: JsonObjectRequest(Request.Method.GET, gameListURL, null, Response.Listener { response ->
+                try {
+                    val jsonArray = response.getJSONArray("")
+                    for( i in 0 until jsonArray.length()){
+                        val gameID = jsonArray.getJSONObject(i)
+                        Log.i("cadena", gameID.toString())
+                    }
+                }catch (e:Exception){
+                    Log.i("cadena", "Error al abrir json object")
+                }
+
+        }, Response.ErrorListener { error ->
+            error.printStackTrace()
+        })
+        {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["X-Riot-Token"] = "RGAPI-c96ebbd1-05e2-448e-88ea-4dc33668a860"
+                return headers
+            }
+        }
+        queue.add(jsonRequest)
     }
 
     private fun getIcon(iconID:Int){
@@ -68,6 +97,31 @@ class MainActivity : AppCompatActivity() {
         }, 0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.ARGB_8888,
             { _ -> })
         queue.add(imageRequest)
+
+
+        /**
+         * [
+        "LA1_1250871565",
+        "LA1_1247354652",
+        "LA1_1247344758",
+        "LA1_1246911582",
+        "LA1_1246899273",
+        "LA1_1246527875",
+        "LA1_1245250024",
+        "LA1_1245206410",
+        "LA1_1245191985",
+        "LA1_1244501218"
+        ]
+
+        Contador de request en una busqueda con riot api
+            summoner: 1
+            lista de n partidas: 1
+            buscar las partidas: n
+
+        imagenes cuentan con otro contador ya que son de ddragon
+
+
+         */
     }
 
 }
